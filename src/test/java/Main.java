bc86b426c3ab5548d62a322c058b169233c65ee3
Nikhil-Venket <nikhilvenket25@gmail.com>
2022-09-29 11:41:28 +0530

    Automation

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,12 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class Main {
     public static void main (String args[])
@@ -9,12 +14,29 @@ public class Main {
         System.out.println("\n >>> Nikhil");
         System.setProperty("webdriver.chrome.driver","C:\\Users\\nikhil.venket\\IdeaProjects\\untitled\\Driver\\chromedriver.exe");
         WebDriver Driver = new ChromeDriver();
-        Driver.get("https://www.google.com");
+        Driver.get("http://testleaf.herokuapp.com/");
+
+        /*Action Class*/
+        Actions act = new Actions(Driver);
+        act.moveToElement(Driver.findElement(By.linkText("Edit")));
+        WebElement drp = Driver.findElement(By.ByXPath"//input[@id=\"email"]"));
+        act.build().perform();
+
+        WebElement drp = Driver.findElement(By.xpath("//img[@alt='ListBox']"));
+        drp.click();
+        /*Select class*/
+        Select sel = new Select(Driver.findElement(By.id("dropdown1")));
+        sel.selectByIndex(1);
+
+        List<WebElement> option = sel.getOptions();
+        for (WebElement ele : option) {
+            System.out.println(ele.getText());
+        }
 
 
-        Driver.findElement(By.xpath("//input[@class=\"gLFyf gsfi\"]")).sendKeys("youtube"+Keys.ENTER);
-        Driver.findElement(By.xpath("//div[@class=\"TbwUpd NJjxre\"]")).click();
-        Driver.findElement(By.xpath("//input[@id=\"search\"]")).sendKeys("Selenium"+Keys.ENTER);
+       // Driver.findElement(By.xpath("//input[@class=\"gLFyf gsfi\"]")).sendKeys("youtube"+Keys.ENTER);
+        //Driver.findElement(By.xpath("//div[@class=\"TbwUpd NJjxre\"]")).click();
+        //Driver.findElement(By.xpath("//input[@id=\"search\"]")).sendKeys("Selenium"+Keys.ENTER);
 
     }
 }
